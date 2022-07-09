ea13ff5fab4391d7b7f6b3b2ed0b759f58cb4304
Phon1209 <ptphon@gmail.com>
2022-07-09 09:51:39 -0400

    Change title of the app

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <BrowserRouter>
       <div className="bg-white h-screen relative flex flex-col">
         <header
-          className="flex flex-row justify-between items-center 
-                    fixed top-0 left-0 p-3 w-full z-10
-                    text-lg font-extrabold bg-white shadow-md"
+          className="flex flex-row justify-center items-center 
+                    fixed top-0 left-0 p-3 pt-6 w-full z-10
+                    text-lg font-extrabold bg-white"
         >
           <Link to="/">
-            <span className="text-primary">CW</span>
-            <span className="text-[#D07D00]">Express</span>
+            <span className="text-[#45A0A6]">CW</span>
+            <span className="text-[#4B5768]">Express</span>
           </Link>
         </header>
         <main className="mt-6 pt-6 pb-10 px-8 h-full w-full">
