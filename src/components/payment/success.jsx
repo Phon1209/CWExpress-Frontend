20f09c59e93f95b11599c8ef203a2a1c3919d4a7
Phon1209 <ptphon@gmail.com>
2022-07-17 03:00:17 -0400

    Create dummy success page

---
+++ src/components/payment/success.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const SuccessPayment = (props) => {
+  // @TODO: Create this page
+
+  return <div>SuccessPayment</div>;
+};
+
+SuccessPayment.propTypes = {};
+
+export default SuccessPayment;
