d63279250581ae10c871cd82d51a1292e4fd6d91
Phon1209 <ptphon@gmail.com>
2022-07-17 05:16:44 -0400

    Silence warning

--- src/components/machine/machine.jsx
+++ src/components/machine/machine.jsx
@@ -16,6 +16,7 @@ const Machine = () => {
   const navigate = useNavigate();
   useEffect(() => {
     getMachine(machineID);
+    // eslint-disable-next-line
   }, [machineID]);
 
   return isLoading ? (
