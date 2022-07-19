e9cd9f35f5129b35a591ed26717590c078d12c47
Phon1209 <ptphon@gmail.com>
2022-07-19 01:52:04 -0400

    Change loading screen to take up all screen

--- src/components/utils/loading.js
+++ src/components/utils/loading.js
@@ -3,7 +3,9 @@ import loading from "./assets/loading.svg";
 
 const Loading = () => {
   return (
-    <img src={loading} alt="loading..." className="w-20 h-20 m-auto"></img>
+    <div className="w-full h-full flex justify-center items-center">
+      <img src={loading} alt="loading..." className="w-20 h-20 m-auto"></img>
+    </div>
   );
 };
 
