c000f5c69e4dc16cebca5c5d350420772e944366
Phon1209 <ptphon@gmail.com>
2022-07-22 06:21:21 -0400

    Customaizable Information component
    - add classes props to className in the component
    - add option to choose light or dark shade of title

--- src/components/layouts/information.jsx
+++ src/components/layouts/information.jsx
@@ -1,15 +1,19 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
 
-const Information = ({ datas }) => {
+const Information = ({ datas, classes, lightTitle }) => {
   return (
-    <section className="grid grid-cols-2 grid-rows-3 gap-y-1 gap-x-5 mt-8 mb-4">
+    <section
+      className={`grid grid-cols-2 grid-rows-3 gap-y-1 gap-x-5 mt-8 mb-4 ${classes}`}
+    >
       {datas.map((data) => {
         return (
           <Fragment key={data.title}>
             <div
               key={data.title + "-title"}
-              className="text-right text-[#325D67]"
+              className={`text-right ${
+                lightTitle ? "text-primary500" : "text-primary600"
+              }`}
             >
               {data.title}
             </div>
@@ -23,8 +27,14 @@ const Information = ({ datas }) => {
   );
 };
 
+Information.defaultProps = {
+  lightTitle: false,
+};
+
 Information.propTypes = {
   datas: PropTypes.array.isRequired,
+  classes: PropTypes.string,
+  lightTitle: PropTypes.bool,
 };
 
 export default Information;
